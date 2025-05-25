d7cb33e87f1ed39638e81601e182eae27ac3d7d5
phonorad <38742894+phonorad@users.noreply.github.com>
2025-05-24 23:00:21 -0400

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -24,7 +24,7 @@ import vga1_16x16 as font_lg
 import vga1_16x32 as font_huge
 
 # === Software Version ===
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 # ========================
 
 # === Definitons for Wifi Setup and Access ===
